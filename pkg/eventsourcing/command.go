56ee0cf27ac21315533bfc23fb755fe8c1f83e52
david terranova <david@smallpdf.com>
2023-11-08 16:58:00 +0400

    command issued by

--- pkg/eventsourcing/command.go
+++ pkg/eventsourcing/command.go
@@ -3,6 +3,7 @@ package eventsourcing
 import (
 	"time"
 
+	"github.com/davidterranova/contacts/pkg/user"
 	"github.com/google/uuid"
 )
 
@@ -10,6 +11,7 @@ type Command[T Aggregate] interface {
 	AggregateId() uuid.UUID
 	AggregateType() AggregateType
 	CreatedAt() time.Time
+	IssuedBy() user.User
 
 	// Check for validity of command on aggregate, mutate the aggregate and return newly emitted events
 	Apply(T) ([]Event[T], error)
@@ -19,12 +21,14 @@ type BaseCommand[T Aggregate] struct {
 	BCAggregateId   uuid.UUID     `validate:"required"`
 	BCAggregateType AggregateType `validate:"required"`
 	BCCreatedAt     time.Time     `validate:"required"`
+	BCIssuedBy      user.User     `validate:"required"`
 }
 
-func NewBaseCommand[T Aggregate](aggregateId uuid.UUID, aggregateType AggregateType) BaseCommand[T] {
+func NewBaseCommand[T Aggregate](aggregateId uuid.UUID, aggregateType AggregateType, issuedBy user.User) BaseCommand[T] {
 	return BaseCommand[T]{
 		BCAggregateId:   aggregateId,
 		BCAggregateType: aggregateType,
+		BCIssuedBy:      issuedBy,
 		BCCreatedAt:     time.Now().UTC(),
 	}
 }
@@ -39,4 +43,8 @@ func (c BaseCommand[T]) AggregateType() AggregateType {
 
 func (c BaseCommand[T]) CreatedAt() time.Time {
 	return c.BCCreatedAt
+}
+
+func (c BaseCommand[T]) IssuedBy() user.User {
+	return c.BCIssuedBy
 }
