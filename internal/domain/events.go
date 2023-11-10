b50fd7afc08c4b0ed6ba529eb512d35b46dcd2ff
david terranova <david@smallpdf.com>
2023-11-10 19:09:15 +0400

    expose events properties to support marshalling

--- internal/domain/events.go
+++ internal/domain/events.go
@@ -16,12 +16,12 @@ const (
 )
 
 type EvtContactCreated struct {
-	eventsourcing.EventBase[*Contact]
+	eventsourcing.EventBase[Contact]
 }
 
 func NewEvtContactCreated(aggregateId uuid.UUID, createdBy user.User) *EvtContactCreated {
 	return &EvtContactCreated{
-		EventBase: eventsourcing.NewEventBase[*Contact](AggregateContact, ContactCreated, aggregateId, createdBy),
+		EventBase: eventsourcing.NewEventBase[Contact](AggregateContact, ContactCreated, aggregateId, createdBy),
 	}
 }
 
@@ -35,75 +35,75 @@ func (e EvtContactCreated) Apply(contact *Contact) error {
 }
 
 type EvtContactEmailUpdated struct {
-	eventsourcing.EventBase[*Contact]
+	eventsourcing.EventBase[Contact]
 
-	email string
+	Email string `json:"email"`
 }
 
 func NewEvtContactEmailUpdated(aggregateId uuid.UUID, updatedBy user.User, email string) *EvtContactEmailUpdated {
 	return &EvtContactEmailUpdated{
-		EventBase: eventsourcing.NewEventBase[*Contact](AggregateContact, ContactEmailUpdated, aggregateId, updatedBy),
-		email:     email,
+		EventBase: eventsourcing.NewEventBase[Contact](AggregateContact, ContactEmailUpdated, aggregateId, updatedBy),
+		Email:     email,
 	}
 }
 
 func (e EvtContactEmailUpdated) Apply(contact *Contact) error {
 	contact.UpdatedAt = e.IssuedAt()
-	contact.Email = e.email
+	contact.Email = e.Email
 
 	return nil
 }
 
 type EvtContactNameUpdated struct {
-	eventsourcing.EventBase[*Contact]
+	eventsourcing.EventBase[Contact]
 
-	firstName string
-	lastName  string
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 }
 
 func NewEvtContactNameUpdated(aggregateId uuid.UUID, updatedBy user.User, firstName string, lastName string) *EvtContactNameUpdated {
 	return &EvtContactNameUpdated{
-		EventBase: eventsourcing.NewEventBase[*Contact](AggregateContact, ContactNameUpdated, aggregateId, updatedBy),
-		firstName: firstName,
-		lastName:  lastName,
+		EventBase: eventsourcing.NewEventBase[Contact](AggregateContact, ContactNameUpdated, aggregateId, updatedBy),
+		FirstName: firstName,
+		LastName:  lastName,
 	}
 }
 
 func (e EvtContactNameUpdated) Apply(contact *Contact) error {
 	contact.UpdatedAt = e.IssuedAt()
-	contact.FirstName = e.firstName
-	contact.LastName = e.lastName
+	contact.FirstName = e.FirstName
+	contact.LastName = e.LastName
 
 	return nil
 }
 
 type EvtContactPhoneUpdated struct {
-	eventsourcing.EventBase[*Contact]
+	eventsourcing.EventBase[Contact]
 
-	phone string
+	Phone string `json:"phone"`
 }
 
 func NewEvtContactPhoneUpdated(aggregateId uuid.UUID, updatedBy user.User, phone string) *EvtContactPhoneUpdated {
 	return &EvtContactPhoneUpdated{
-		EventBase: eventsourcing.NewEventBase[*Contact](AggregateContact, ContactPhoneUpdated, aggregateId, updatedBy),
-		phone:     phone,
+		EventBase: eventsourcing.NewEventBase[Contact](AggregateContact, ContactPhoneUpdated, aggregateId, updatedBy),
+		Phone:     phone,
 	}
 }
 
 func (e EvtContactPhoneUpdated) Apply(contact *Contact) error {
 	contact.UpdatedAt = e.IssuedAt()
-	contact.Phone = e.phone
+	contact.Phone = e.Phone
 
 	return nil
 }
 
 type EvtContactDeleted struct {
-	eventsourcing.EventBase[*Contact]
+	eventsourcing.EventBase[Contact]
 }
 
 func NewEvtContactDeleted(aggregateId uuid.UUID, deletedBy user.User) *EvtContactDeleted {
 	return &EvtContactDeleted{
-		EventBase: eventsourcing.NewEventBase[*Contact](AggregateContact, ContactDeleted, aggregateId, deletedBy),
+		EventBase: eventsourcing.NewEventBase[Contact](AggregateContact, ContactDeleted, aggregateId, deletedBy),
 	}
 }
 
