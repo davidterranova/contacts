0e0ab177c547ebde20ec6b07d4a634b1f6345819
david terranova <david@smallpdf.com>
2023-11-14 18:28:10 +0400

    extract interface from implementation

--- pkg/eventsourcing/inmemory_event_store.go
+++ pkg/eventsourcing/inmemory_event_store.go
@@ -7,18 +7,6 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-type EventStore[T Aggregate] interface {
-	// Store events
-	Store(ctx context.Context, events ...Event[T]) error
-	// Load events from the given aggregate
-	Load(ctx context.Context, aggregateType AggregateType, aggregateId uuid.UUID) ([]Event[T], error) // TODO: remove aggregateType
-
-	// LoadUnpublished loads a batch of un published events
-	LoadUnpublished(ctx context.Context, batchSize int) ([]Event[T], error)
-	// MarkPublished marks events as published
-	MarkPublished(ctx context.Context, events ...Event[T]) error
-}
-
 type eventStore[T Aggregate] struct {
 	aggregateEvents   map[uuid.UUID][]Event[T]
 	unPublishedEvents []Event[T]
